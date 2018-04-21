beca517010cecea18efebd78fc347f0c80bf591c
Yiyi Wang <shd101wyy@gmail.com>
2018-04-21 15:48:45 -0500

    Update smart contract code to support tagState, userState, as well as mentions

--- frontend/smartcontract/ribbit.prod.0.sol
+++ frontend/smartcontract/ribbit.prod.0.sol
@@ -9,21 +9,43 @@ contract Ribbit {
      * 0x1 => likes 
      * 0x2 => dislikes
      * 0x3 => reports
-     * 0x4 => comments
+     * 0x4 => replies
      * 0x5 => reposts
      * 
      * transactionHash => field => value
      */
-    mapping (bytes32 => mapping(uint => uint)) public state;   // transactionHash
+    mapping (bytes32 => mapping(uint => uint)) public state;
+
+    /**
+     * 0x0 => likes
+     * 0x1 => dislikes
+     * 0x2 => reports
+     * tagHash => field => value
+     */
+    mapping (bytes32 => mapping(uint => uint)) public tagState;
+
+    /**
+     * 0x0 => likes
+     * 0x1 => dislikes
+     * 0x2 => reports
+     * userAddress => field => value
+     */
+    mapping (address => mapping(uint => uint)) public userState;
+
+    /**
+     * User metadata.
+     * For example, user displayName, profileImage, cover...
+     */
     mapping (address => string) public metaDataJSONStringMap;  
     
     /**
      * uint[2] here 
      *     [0] => block.number
-     *     [1] => hash (sha256 hash)
+     *     [1] => hash (sha256 hash) || transactionHash if block.number == 0
      */
     mapping (address => uint[2]) public currentFeedInfoMap;
-    mapping (bytes32 => uint[2]) public currentCommentInfoMap;
+    mapping (bytes32 => uint[2]) public currentReplyInfoMap;
+    // mapping (address => uint[2]) public currentMentionInfoMap; // => combined with currentTagInfoByTimeMap;
     mapping (bytes32 => uint[2]) public currentTagInfoByTimeMap;
     mapping (bytes32 => uint[2]) public currentTagInfoByTrendMap;
 
@@ -66,14 +88,15 @@ contract Ribbit {
     function getCurrentTagInfoByTrend(bytes32 tag) external constant returns (uint[2]) {
         return currentTagInfoByTrendMap[tag];
     }
-    function getCurrentCommentInfo(bytes32 parentTransactionHash) external constant returns (uint[2]) {
-        return currentCommentInfoMap[parentTransactionHash];
+    function getCurrentReplyInfo(bytes32 parentTransactionHash) external constant returns (uint[2]) {
+        return currentReplyInfoMap[parentTransactionHash];
     }
     
     // Post Feed 
     event PostEvent(uint[2] previousFeedTransactionInfo);
     event SavePreviousTagInfoByTimeEvent(bytes32 tag, uint[2] previousTagInfoByTime);
     event SavePreviousTagInfoByTrendEvent(bytes32 tag, uint[2] previousTagInfoByTrend);
+    event SavePreviousMentionEvent(address mention, uint[2] previousMentionInfo);
     function post(uint version, uint timestamp, string message, uint messageHash, bytes32 previousFeedTransactionHash, bytes32[] tags) external {
         emit PostEvent(currentFeedInfoMap[msg.sender]);
         uint blockNumber = block.number;
@@ -81,15 +104,18 @@ contract Ribbit {
         currentFeedInfoMap[msg.sender][1] = messageHash;
 
         bytes32 tag;
-        for (uint i = 0; i < tags.length; i++) {
+        uint i;
+        for (i = 0; i < tags.length; i++) {
             tag = tags[i];
             emit SavePreviousTagInfoByTimeEvent(tag, currentTagInfoByTimeMap[tag]);
             currentTagInfoByTimeMap[tag][0] = blockNumber;
             currentTagInfoByTimeMap[tag][1] = messageHash;
             
-            emit SavePreviousTagInfoByTrendEvent(tag, currentTagInfoByTrendMap[tag]);
-            currentTagInfoByTrendMap[tag][0] = blockNumber;
-            currentTagInfoByTrendMap[tag][1] = messageHash;
+            if (tag >> 20 != 0x0) { // this tag is not a user address.
+                emit SavePreviousTagInfoByTrendEvent(tag, currentTagInfoByTrendMap[tag]);
+                currentTagInfoByTrendMap[tag][0] = blockNumber;
+                currentTagInfoByTrendMap[tag][1] = messageHash;
+            }
         }
     }
 
@@ -114,13 +140,14 @@ contract Ribbit {
         }
     }
     
-    // Make comment
-    event CommentEvent(uint[2] previousCommentInfo);
-    function comment(uint version, uint timestamp, bytes32 parentTransactionHash, uint parentTransactionBlockNumber, uint parentTransactionMessageHash, string message, uint messageHash, bytes32 previousCommentTransactionHash, bytes32[] tags) external {
-        emit CommentEvent(currentCommentInfoMap[parentTransactionHash]);
-        currentCommentInfoMap[parentTransactionHash][0] = block.number;   
-        currentCommentInfoMap[parentTransactionHash][1] = messageHash;
-        state[parentTransactionHash][4] = state[parentTransactionHash][4] + 1; // increase number of comments
+    // Reply Feed
+    event ReplyEvent(uint[2] previousReplyInfo);
+    function reply(uint version, uint timestamp, bytes32 parentTransactionHash, uint parentTransactionBlockNumber, uint parentTransactionMessageHash, string message, uint messageHash, bytes32 previousReplyTransactionHash, bytes32[] tags) external {
+        emit ReplyEvent(currentReplyInfoMap[parentTransactionHash]);
+        uint blockNumber = block.number;
+        currentReplyInfoMap[parentTransactionHash][0] = blockNumber;   
+        currentReplyInfoMap[parentTransactionHash][1] = messageHash;
+        state[parentTransactionHash][4] = state[parentTransactionHash][4] + 1; // increase number of replies
         
         bytes32 tag;
         for (uint i = 0; i < tags.length; i++) {
@@ -130,18 +157,22 @@ contract Ribbit {
                 currentTagInfoByTrendMap[tag][0] = parentTransactionBlockNumber;
                 currentTagInfoByTrendMap[tag][1] = parentTransactionMessageHash;
             }
+            if (tag >> 20 == 0) { // it's a user address
+                currentTagInfoByTimeMap[tag][0] = blockNumber;
+                currentTagInfoByTimeMap[tag][1] = messageHash;
+            }
         }
     }
 
-    // Repost and Comment
-    event RepostAndCommentEvent(uint[2] previousFeedTransactionInfo, uint[2] previousCommentInfo);
-    function repostAndComment(uint version, uint timestamp, bytes32 parentTransactionHash, uint parentTransactionBlockNumber, uint parentTransactionMessageHash, string message, uint messageHash, bytes32 previousCommentTransactionHash, bytes32[] tags) external {
-        emit RepostAndCommentEvent(currentFeedInfoMap[msg.sender], currentCommentInfoMap[parentTransactionHash]);
+    // Repost and Reply
+    event RepostAndReplyEvent(uint[2] previousFeedTransactionInfo, uint[2] previousReplyInfo);
+    function repostAndReply(uint version, uint timestamp, bytes32 parentTransactionHash, uint parentTransactionBlockNumber, uint parentTransactionMessageHash, string message, uint messageHash, bytes32 previousReplyTransactionHash, bytes32[] tags) external {
+        emit RepostAndReplyEvent(currentFeedInfoMap[msg.sender], currentReplyInfoMap[parentTransactionHash]);
         uint blockNumber = block.number;
         
-        currentCommentInfoMap[parentTransactionHash][0] = blockNumber;   
-        currentCommentInfoMap[parentTransactionHash][1] = messageHash;
-        state[parentTransactionHash][4] = state[parentTransactionHash][4] + 1; // increase number of comments
+        currentReplyInfoMap[parentTransactionHash][0] = blockNumber;   
+        currentReplyInfoMap[parentTransactionHash][1] = messageHash;
+        state[parentTransactionHash][4] = state[parentTransactionHash][4] + 1; // increase number of replies
     
         currentFeedInfoMap[msg.sender][0] = blockNumber;
         currentFeedInfoMap[msg.sender][1] = messageHash;
@@ -154,6 +185,11 @@ contract Ribbit {
                 emit SavePreviousTagInfoByTrendEvent(tag, currentTagInfoByTrendMap[tag]);
                 currentTagInfoByTrendMap[tag][0] = parentTransactionBlockNumber;
                 currentTagInfoByTrendMap[tag][1] = parentTransactionMessageHash;
+            }
+
+            if (tag >> 20 == 0) { // it's a user address
+                currentTagInfoByTimeMap[tag][0] = blockNumber;
+                currentTagInfoByTimeMap[tag][1] = messageHash;
             }
         }
     }
@@ -183,4 +219,35 @@ contract Ribbit {
     function report(bytes32 transactionHash) external {
         state[transactionHash][3] = state[transactionHash][3] + 1;
     }
+
+    // Like tag
+    function likeTag(bytes32 tag) external {
+        tagState[tag][0] = tagState[tag][0] + 1;
+    }
+
+    // Dislike tag
+    function dislikeTag(bytes32 tag) external {
+        tagState[tag][1] = tagState[tag][1] + 1;
+    }
+
+    // Report tag
+    function reportTag(bytes32 tag) external {
+        tagState[tag][2] = tagState[tag][2] + 1;
+    }
+
+    // Like user
+    function likeUser(address userAddress) external {
+        userState[userAddress][0] = userState[userAddress][0] + 1;
+    }
+
+    // Dislike user
+    function dislikeUser(address userAddress) external {
+        userState[userAddress][1] = userState[userAddress][1] + 1;
+    }
+    
+    // Report user
+    function reportUser(address userAddress) external {
+        userState[userAddress][2] = userState[userAddress][2] + 1;
+    }
+    
 }
